bafe03e145229d6cb5a5c60088110249b28ba20f
Magnus Landauer <magnuslandauer@outlook.com>
2024-01-15 19:37:51 -0700

    updated config to support github images

--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // domains: [
-    //   "images.unsplash.com",
-    //   "lh3.googleusercontent.com",
-    //   "avatars.githubusercontent.com",
-    // ],
+    domains: [
+      "images.unsplash.com",
+      "lh3.googleusercontent.com",
+      "avatars.githubusercontent.com",
+    ],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "*.googleusercontent.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "*.githubusercontent.com",
         port: "",
         pathname: "**",
       },
